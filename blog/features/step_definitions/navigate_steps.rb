3bc3d799a5e3266305e25e54effa9e9ffddb68a4
bugbare <rshah@bugbare.co.uk>
2015-12-07 23:54:04 +0000

    Added Rspec, created home feature and steps

---
+++ blog/features/step_definitions/navigate_steps.rb
@@ -0,0 +1,20 @@
+#Given(/^the articulate site is available$/) do
+#  pending # express the regexp above with the code you wish you had
+#end
+
+#When(/^I visit the home page$/) do
+#  pending # express the regexp above with the code you wish you had
+#end
+
+Given(/^I have loaded some articles onto the articulate site$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I visit the home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the confirmation message to destroy the item appears$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
